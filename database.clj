161180d2ff15b47a9e6b7ad63f8b7436f35ebf05
sernamar <sernamar@protonmail.com>
2022-02-13 17:24:35 +0100

    Use an in-memory SQLite database instead of a local one

--- database.clj
+++ database.clj
@@ -3,21 +3,19 @@
             [next.jdbc.sql :refer [insert!]]))
 
 (def database {:dbtype "sqlite"
-               :dbname "database/logs.db"})
+               :dbname ":memory:"})
 
 (def datasource (jdbc/get-datasource database))
+(def connection (jdbc/get-connection datasource))
 
 (defn test-database []
-  (with-open [connection (jdbc/get-connection datasource)]
-    (jdbc/execute-one! connection ["SELECT 1 + 2"])))
+  (jdbc/execute-one! connection ["SELECT 1 + 2"]))
 
 (defn create-table []
-  (with-open [connection (jdbc/get-connection datasource)]
-    (jdbc/execute-one! connection ["CREATE TABLE log (id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, date TEXT, time TEXT, zone TEXT, cik TEXT, accession TEXT, doc TEXT, code TEXT, size TEXT, idx TEXT, norefer TEXT, noagent TEXT, find TEXT, crawler TEXT, browser TEXT)"])))
+  (jdbc/execute-one! connection ["CREATE TABLE log (id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, date TEXT, time TEXT, zone TEXT, cik TEXT, accession TEXT, doc TEXT, code TEXT, size TEXT, idx TEXT, norefer TEXT, noagent TEXT, find TEXT, crawler TEXT, browser TEXT)"]))
 
 (defn insert-log []
-  (with-open [connection (jdbc/get-connection datasource)]
-    (insert! connection 'log {:ip "1.234.83.eib"
+  (insert! connection 'log {:ip "1.234.83.eib"
                               :date "2017-04-06"
                               :time "00:00:00"
                               :zone "0.0"
@@ -31,12 +29,10 @@
                               :noagent "0.0"
                               :find "10.0"
                               :crawler "0.0"
-                              :browser "fox"})))
+                              :browser "fox"}))
 
 (defn select-all []
-  (with-open [connection (jdbc/get-connection datasource)]
-    (jdbc/execute! connection ["SELECT * FROM log"])))
+  (jdbc/execute! connection ["SELECT * FROM log"]))
 
 (defn drop-table []
-  (with-open [connection (jdbc/get-connection datasource)]
-    (jdbc/execute-one! connection ["DROP TABLE log"])))
+  (jdbc/execute-one! connection ["DROP TABLE log"]))
