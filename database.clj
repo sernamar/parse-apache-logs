caa383ac318d30d53bb8ecaf26e3f05344780da0
sernamar <sernamar@protonmail.com>
2022-02-13 21:49:34 +0100

    Reorganize database functions

--- database.clj
+++ database.clj
@@ -2,37 +2,43 @@
   (:require [next.jdbc :as jdbc]
             [next.jdbc.sql :as jdbc.sql]))
 
-(def database {:dbtype "sqlite"
-               :dbname ":memory:"})
+(def connection (atom nil))
 
-(def datasource (jdbc/get-datasource database))
-(def connection (jdbc/get-connection datasource))
+(defn- create-database []
+  (let [database {:dbtype "sqlite" :dbname ":memory:"}]
+    (reset! connection (-> database
+                           jdbc/get-datasource
+                           jdbc/get-connection))))
 
-(defn test-database []
-  (jdbc/execute-one! connection ["SELECT 1 + 2"]))
+(defn- create-log-table []
+  (jdbc/execute-one! @connection ["CREATE TABLE log (id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, date TEXT, time TEXT, zone TEXT, cik TEXT, accession TEXT, doc TEXT, code TEXT, size TEXT, idx TEXT, norefer TEXT, noagent TEXT, find TEXT, crawler TEXT, browser TEXT)"]))
 
-(defn create-table []
-  (jdbc/execute-one! connection ["CREATE TABLE log (id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, date TEXT, time TEXT, zone TEXT, cik TEXT, accession TEXT, doc TEXT, code TEXT, size TEXT, idx TEXT, norefer TEXT, noagent TEXT, find TEXT, crawler TEXT, browser TEXT)"]))
+(defn- insert-log []
+  (jdbc.sql/insert! @connection 'log {:ip "1.234.83.eib"
+                                      :date "2017-04-06"
+                                      :time "00:00:00"
+                                      :zone "0.0"
+                                      :cik "1319947.0"
+                                      :accession "0001225208-17-007392"
+                                      :doc "-index.htm"
+                                      :code "200.0"
+                                      :size "2770.0"
+                                      :idx "1.0"
+                                      :norefer "0.0"
+                                      :noagent "0.0"
+                                      :find "10.0"
+                                      :crawler "0.0"
+                                      :browser "fox"}))
 
-(defn insert-log []
-  (jdbc.sql/insert! connection 'log {:ip "1.234.83.eib"
-                                     :date "2017-04-06"
-                                     :time "00:00:00"
-                                     :zone "0.0"
-                                     :cik "1319947.0"
-                                     :accession "0001225208-17-007392"
-                                     :doc "-index.htm"
-                                     :code "200.0"
-                                     :size "2770.0"
-                                     :idx "1.0"
-                                     :norefer "0.0"
-                                     :noagent "0.0"
-                                     :find "10.0"
-                                     :crawler "0.0"
-                                     :browser "fox"}))
+(defn- select-all []
+  (jdbc/execute! @connection ["SELECT * FROM log"]))
 
-(defn select-all []
-  (jdbc/execute! connection ["SELECT * FROM log"]))
+(defn- close-connection []
+  (.close @connection))
 
-(defn drop-table []
-  (jdbc/execute-one! connection ["DROP TABLE log"]))
+(defn initialize-database []
+  (create-database)
+  (create-log-table))
+
+(defn close-database []
+  (close-connection))
