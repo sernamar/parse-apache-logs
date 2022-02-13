14fb0bd8491dca9059e855d2ebc730d7a5c0a86e
sernamar <sernamar@protonmail.com>
2022-02-13 20:53:22 +0100

    Be explicit when using the INSERT! function

--- database.clj
+++ database.clj
@@ -1,6 +1,6 @@
 (ns parse-apache-logs.database
   (:require [next.jdbc :as jdbc]
-            [next.jdbc.sql :refer [insert!]]))
+            [next.jdbc.sql :as jdbc.sql]))
 
 (def database {:dbtype "sqlite"
                :dbname ":memory:"})
@@ -15,21 +15,21 @@
   (jdbc/execute-one! connection ["CREATE TABLE log (id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, date TEXT, time TEXT, zone TEXT, cik TEXT, accession TEXT, doc TEXT, code TEXT, size TEXT, idx TEXT, norefer TEXT, noagent TEXT, find TEXT, crawler TEXT, browser TEXT)"]))
 
 (defn insert-log []
-  (insert! connection 'log {:ip "1.234.83.eib"
-                              :date "2017-04-06"
-                              :time "00:00:00"
-                              :zone "0.0"
-                              :cik "1319947.0"
-                              :accession "0001225208-17-007392"
-                              :doc "-index.htm"
-                              :code "200.0"
-                              :size "2770.0"
-                              :idx "1.0"
-                              :norefer "0.0"
-                              :noagent "0.0"
-                              :find "10.0"
-                              :crawler "0.0"
-                              :browser "fox"}))
+  (jdbc.sql/insert! connection 'log {:ip "1.234.83.eib"
+                                     :date "2017-04-06"
+                                     :time "00:00:00"
+                                     :zone "0.0"
+                                     :cik "1319947.0"
+                                     :accession "0001225208-17-007392"
+                                     :doc "-index.htm"
+                                     :code "200.0"
+                                     :size "2770.0"
+                                     :idx "1.0"
+                                     :norefer "0.0"
+                                     :noagent "0.0"
+                                     :find "10.0"
+                                     :crawler "0.0"
+                                     :browser "fox"}))
 
 (defn select-all []
   (jdbc/execute! connection ["SELECT * FROM log"]))
