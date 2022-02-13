c042b211b622b2e79521d7f245510f3cb2c8ec1c
sernamar <sernamar@protonmail.com>
2022-02-13 10:46:31 +0100

    Delete examples and innecesary functions

--- parser.clj
+++ parser.clj
@@ -13,27 +13,10 @@
                       (do (.close reader) nil))))]
     (helper (clojure.java.io/reader file))))
 
-;; example
-(let [file "data/log20170406/10.log"]
-  (count (read-lines-lazily file))) ;=> 10
-
-(defn dummy-parser
-  "Returns the same string it receives."
-  [string]
-  string)
-
 (defn parse-file
   "Parse the file lazily using the parser function."
   [file parser]
   (parser (read-lines-lazily file)))
-
-;; example
-(let [file "data/log20170406/10.log"]
-  (parse-file file dummy-parser))
-
-;; Apache log format:
-;; ip,date,time,zone,cik,accession,doc,code,size,idx,norefer,noagent,find,crawler,browser
-;; 1.234.83.eib,2017-04-06,00:00:00,0.0,1319947.0,0001225208-17-007392,-index.htm,200.0,2770.0,1.0,0.0,0.0,10.0,0.0,fox
 
 (def apache-log-pattern
   (let [ip-regex #"(\d{1,3}\.\d{1,3}\.\d{1,3}.\w{1,3})"
