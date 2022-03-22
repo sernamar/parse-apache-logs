1beab61976f35226206c7d845bfcb4cd7fa0e2dd
sernamar <sernamar@protonmail.com>
2022-03-22 09:56:43 +0100

    Add missing docstrings

--- src/database.clj
+++ src/database.clj
@@ -8,20 +8,22 @@
                    slurp
                    clojure.edn/read-string))
 
-(defn- get-database-connection []
-  (let [database db-params]
-    (reset! connection (-> database
+(defn- get-database-connection [db-params]
+  "Get a connection to the database using the provided connection parameters."
+  (reset! connection (-> db-params
                            jdbc/get-datasource
-                           jdbc/get-connection))))
+                           jdbc/get-connection)))
 
 ;;; ------------------- ;;;
 ;;; Database connection ;;;
 ;;; ------------------- ;;;
 
 (defn- create-log-table []
+  "Create a table called 'log' into the database."
   (jdbc/execute-one! @connection ["CREATE TABLE log (id SERIAL PRIMARY KEY, ip TEXT, date TEXT, time TEXT, zone TEXT, cik TEXT, accession TEXT, doc TEXT, code TEXT, size TEXT, idx TEXT, norefer TEXT, noagent TEXT, find TEXT, crawler TEXT, browser TEXT)"]))
 
 (defn- table-in-database? [table]
+  "Check in a table exists in the database."
   (let [query (str "SELECT EXISTS (SELECT FROM information_schema.tables WHERE  table_schema='public' AND table_name='"
                    table
                    "')")]
@@ -29,7 +31,7 @@
 
 (defn connect-to-database []
   "Connect to the database, creating a table called 'log' if it doesn't exist."
-  (get-database-connection)
+  (get-database-connection db-params)
   (when-not (table-in-database? "log")
     (create-log-table)))
 
@@ -42,6 +44,7 @@
 ;;; ---------------- ;;;
 
 (defn- insert-log [log]
+  "Insert a string with the content of a log line into the 'log' table."
   (jdbc.sql/insert! @connection 'log log))
 
 (defn insert-logs [logs]
